90eec9d745a1b3b0560109176633c9eb82a9bb68
UwUrange <74237145+UwUrange@users.noreply.github.com>
2023-02-20 18:26:33 +0100

    added actual site

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  content: [
+    './components/**/*.{html,js}',
+    './pages/**/*.{html,js}',
+    './index.html',
+  ],
+  theme: {
+    fontFamily: {
+      'Gothic': 'CenturyGothic',
+      'GothicBold': 'CenturyGothicBold',
+      'Pacifico': 'Pacifico',
+      'Raleway': 'Raleway',
+
+    }
+  }
+}
